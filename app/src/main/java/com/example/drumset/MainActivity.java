1e1c523f6ccefda84dcf321ec8c39454acc2780e
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-05-14 13:44:35 +0800

    complete

--- app/src/main/java/com/example/drumset/MainActivity.java
+++ app/src/main/java/com/example/drumset/MainActivity.java
@@ -9,8 +9,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SoundManager soundManager;
     private LinearLayout mainLayout;
-    private TextView status;
+
+//    private TextView status;
+
+    private int bassDrum;
+    private int closeHiHat;
+    private int openHiHat;
+    private int cymbal;
+    private int floorTom;
+    private int midTom;
+    private int highTom;
+    private int snareDrum;
 
 
     @Override
@@ -18,51 +29,76 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        status = (TextView) findViewById(R.id.status);
+//        status = (TextView) findViewById(R.id.status);
+
         mainLayout = (LinearLayout) findViewById(R.id.layout);
+
+        soundManager = new SoundManager(this);
+
+        bassDrum = soundManager.addSound(R.raw.bassdrum);
+        closeHiHat = soundManager.addSound(R.raw.closehihatcymbals);
+        openHiHat = soundManager.addSound(R.raw.openhihatcymbals);
+        cymbal = soundManager.addSound(R.raw.cymbal);
+        floorTom = soundManager.addSound(R.raw.floortom);
+        midTom = soundManager.addSound(R.raw.midtom);
+        highTom = soundManager.addSound(R.raw.hightom);
+        snareDrum = soundManager.addSound(R.raw.snaredrum);
+
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
+        if (event.getAction() != MotionEvent.ACTION_DOWN){
+            return super.onTouchEvent(event);
+        }
+
         Position position = getPosition(event.getX(), event.getY());
 
         switch(position){
             case TOP_LEFT1:
-                status.setText("Open Hi-Hat");
+//                status.setText("Open Hi-Hat");
+                soundManager.play(openHiHat);
                 break;
 
             case TOP_LEFT2:
-                status.setText("Close Hi-Hat");
+//                status.setText("Close Hi-Hat");
+                soundManager.play(closeHiHat);
                 break;
 
             case TOP_RIGHT1:
-                status.setText("Cymbal");
+//                status.setText("Cymbal");
+                soundManager.play(cymbal);
                 break;
 
             case TOP_RIGHT2:
-                status.setText("High Tom");
+//                status.setText("High Tom");
+                soundManager.play(highTom);
                 break;
 
             case BOTTOM_LEFT1:
-                status.setText("Snare Drum");
+//                status.setText("Snare Drum");
+                soundManager.play(snareDrum);
                 break;
 
             case BOTTOM_LEFT2:
-                status.setText("Bass Drum");
+//                status.setText("Bass Drum");
+                soundManager.play(bassDrum);
                 break;
 
             case BOTTOM_RIGHT1:
-                status.setText("Mid Tom");
+//                status.setText("Mid Tom");
+                soundManager.play(midTom);
                 break;
 
             case BOTTOM_RIGHT2:
-                status.setText("Open Hi-Hat");
+//                status.setText("Floor Tom");
+                soundManager.play(floorTom);
                 break;
 
             case MIDDLE:
-                status.setText("Floor Tom");
+//                status.setText("None");
                 break;
 
         }
---
+++ app/src/main/java/com/example/drumset/SoundManager.java
@@ -0,0 +1,33 @@
+package com.example.drumset;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.media.SoundPool;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+public class SoundManager {
+    private SoundPool pool;
+    private Context context;
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public SoundManager(Context context){
+        this.context = context;
+        SoundPool.Builder builder = new SoundPool.Builder();
+        builder.setMaxStreams(10);
+        pool = builder.build();
+
+
+
+    }
+
+    public int addSound(int resourceID){
+        return pool.load(context, resourceID, 1);
+    }
+
+    public void play(int soundID){
+        pool.play(soundID, 1,1,1,0, 1);
+
+    }
+
+}
